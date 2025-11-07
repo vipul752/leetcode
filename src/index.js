c6f99d5dfdc75ef96941981df1936ceb2a9060a7
vipul kumar <bca23161@glbim.ac.in>
2025-11-07 21:08:31 +0530

    add FRONTEND URL

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://leetcode-frontend-75.vercel.app",
     credentials: true,
   })
 );
