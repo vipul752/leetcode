dca4ce6fe9aae2d3f6f212d3eaacdbf73a3b5c32
vipul kumar <bca23161@glbim.ac.in>
2025-11-18 17:34:50 +0530

    some Changes

--- src/index.js
+++ src/index.js
@@ -36,8 +36,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://leetcode-frontend-75.vercel.app",
-    // origin: "http://localhost:5173",
+    // origin: "https://leetcode-frontend-75.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
